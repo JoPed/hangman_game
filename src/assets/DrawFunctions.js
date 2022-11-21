025232a47f290449beb4a4116bf5f013f381d08d
JoPed <jonasbjoern@gmail.com>
2022-11-21 10:20:29 +0100

    Made the gallow design a little bit smaller. And the canvas container a little bigger, to contain wrong guesses and win/lose text

--- src/assets/DrawFunctions.js
+++ src/assets/DrawFunctions.js
@@ -27,53 +27,53 @@ const bottomGallow = (ctx) => {
 }
 
 const upwardsGallow = (ctx) => {
-    draw((ctx.canvas.width / 2) - 230, ctx.canvas.height - 50, (ctx.canvas.width / 2) - 230, 50);
+    draw((ctx.canvas.width / 2) - 230, ctx.canvas.height - 50, (ctx.canvas.width / 2) - 230, 125);
 }
 
 const topGallow = (ctx) => {
-    draw((ctx.canvas.width / 2) - 232, 50, (ctx.canvas.width / 2) + 100, 50);
+    draw((ctx.canvas.width / 2) - 232, 125, (ctx.canvas.width / 2) + 100, 125);
 }
 
 const rope = ctx => {
 
-    draw((ctx.canvas.width / 2) + 50, 50, (ctx.canvas.width / 2) + 50, 75 )
+    draw((ctx.canvas.width / 2) + 50, 125, (ctx.canvas.width / 2) + 50, 150)
 
 }
 
 const head = (ctx) => {
     ctx.beginPath();
     let xPos = (ctx.canvas.width / 2) + 50, 
-    yPos = 90, radius = 15;
+    yPos = 165, radius = 15;
     ctx.arc(xPos, yPos, radius, 0, Math.PI * 2);
     ctx.stroke();
 }
 
 const torso = ctx => {
 
-    draw((ctx.canvas.width / 2) + 50, 105, (ctx.canvas.width / 2) + 50, 170);
+    draw((ctx.canvas.width / 2) + 50, 180, (ctx.canvas.width / 2) + 50, 235);
 
 }
 
 const rightArm = ctx => {
 
-    draw((ctx.canvas.width / 2) + 50, 120, (ctx.canvas.width / 2) + 90, 130)
+    draw((ctx.canvas.width / 2) + 50, 195, (ctx.canvas.width / 2) + 90, 205)
 
 }
 
 const leftArm = ctx => {
-    draw((ctx.canvas.width / 2) + 50, 120, (ctx.canvas.width / 2) +10, 130)
+    draw((ctx.canvas.width / 2) + 50, 195, (ctx.canvas.width / 2) +10, 205)
 
 }
 
 const rightLeg = ctx => {
 
-    draw((ctx.canvas.width / 2) + 50, 170, (ctx.canvas.width / 2) + 100, 190)
+    draw((ctx.canvas.width / 2) + 50, 235, (ctx.canvas.width / 2) + 100, 255)
 
 }
 
 const leftLeg = ctx => {
 
-    draw((ctx.canvas.width / 2) + 50, 170, (ctx.canvas.width / 2), 190)
+    draw((ctx.canvas.width / 2) + 50, 235, (ctx.canvas.width / 2), 255)
     
 }
 
